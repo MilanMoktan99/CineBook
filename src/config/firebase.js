209acf7991fb03e6beb169188e0e75e57e7b5691
MilanMoktan99 <milanmok816@gmail.com>
2026-02-04 11:19:47 +0545

    config files with meta env added

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "movie-booking-system-3f407.firebaseapp.com",
-  projectId: "movie-booking-system-3f407",
-  storageBucket: "movie-booking-system-3f407.firebasestorage.app",
-  messagingSenderId: "62889299673",
-  appId: "1:62889299673:web:c20225ab13f52ef127f8c8",
-  measurementId: "G-WSMFZ6Z6RV"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
